286391f97d36cb92385eea3bfee5794e314ec39a
Ali Jahan <ajaha004@ucr.edu>
2024-02-02 12:06:32 -0800

    workloads name changed in build_images script

--- scripts/docker/build_images.sh
+++ scripts/docker/build_images.sh
@@ -3,7 +3,10 @@
 . ./env_setup.sh
 
 # GPU workloads need to be cloned since they are provate repos and need ssh keys to be cloned. 
-if [ $1 = "gpu-workloads-remote-rocm" ]; then
+if [ $1 = "miniMDock" ]; then
+echo "Cloning workloads into /workloads/ ..." && bash ${PROJECT_ROOT}/workloads/clone_workloads.sh ${PROJECT_ROOT}/workloads/
+fi
+if [ $1 = "Inference-Server" ]; then
 echo "Cloning workloads into /workloads/ ..." && bash ${PROJECT_ROOT}/workloads/clone_workloads.sh ${PROJECT_ROOT}/workloads/
 fi
 echo "Building Image: $1 ..." && cd ${PROJECT_ROOT}/.devcontainer/ && docker compose build $1  --no-cache  --progress plain &> ${LOGS_DIR}/$(date -I'minutes')$1_build.log && cd ${PROJECT_ROOT}/scripts/docker && printf "\r\rDONE\n"
